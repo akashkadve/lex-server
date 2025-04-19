bc71086af2d39fcec9e4932c713ac75e5b8280d1
Akash Kadve <akashkadve000@gmail.com>
2025-04-19 15:47:13 +0530

    update

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import userSchema from "../validate/validations.js";
 import validator from "../validate/validator.js";
-import { create } from "../Controllers/controllers.js";
+import create from "../Controllers/Controllers.js";
 
 const route = express.Router();
 route.post("/create", validator(userSchema), create);
